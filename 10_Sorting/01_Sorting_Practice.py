63481d740ec5dd6290a68b3301aa6a449a8412ca
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-09-02 22:50:11 +0530

    Create 01_Sorting_Practice.py

---
+++ 10_Sorting/01_Sorting_Practice.py
@@ -0,0 +1,6 @@
+numbers = [8, 3, 1, 7, 2, 9]
+sorted_a = sorted(numbers)
+print("Original: ",numbers)
+print("Ascending copy: ",sorted_a)
+numbers.sort(reverse =True)
+print("Descending original: ",numbers)
